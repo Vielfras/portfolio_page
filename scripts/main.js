ea0016f2d196013a96b191f32d4e40ac301909b6
vielfras <vielfras@gmail.com>
2023-12-28 17:04:28 +0200

    fixed a bug where pressing the logo icon didn't take back to the main site

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,9 @@
 import {GetProjectPageMainElement} from "./project_page.js"
 import {URLManager} from "./url_manager.js"
 
-const portfolioPageUrl = "oksman.netlify.app";
+const portfolioPageUrl = "https://oksman.netlify.app";
+// const portfolioPageUrl = "http://127.0.0.1:5500/";
+
 const urlManager = new URLManager();
 
 const projectPaths = {
